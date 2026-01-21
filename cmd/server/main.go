4e33f3e4d494d91d0b9d0ff89d4bb516e86a401b
Louis Branch <branch3e8@gmail.com>
2026-01-21 09:53:25 -0500

    Move gRPC server wiring into internal package

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,40 +1,23 @@
 package main
 
 import (
-	"context"
 	"flag"
-	"fmt"
 	"log"
-	"net"
 
-	pb "github.com/louisbranch/duality-protocol/api/gen/go/duality/v1"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
+	"github.com/louisbranch/duality-protocol/internal/server"
 )
 
 var (
 	port = flag.Int("port", 8080, "The server port")
 )
 
-type server struct {
-	pb.UnimplementedDiceRollServiceServer
-}
-
-func (s *server) ActionRoll(ctx context.Context, in *pb.ActionRollRequest) (*pb.ActionRollResponse, error) {
-	return nil, status.Error(codes.Unimplemented, "ActionRoll is not implemented yet")
-}
-
 func main() {
 	flag.Parse()
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	grpcServer, err := server.New(*port)
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		log.Fatalf("failed to initialize server: %v", err)
 	}
-	s := grpc.NewServer()
-	pb.RegisterDiceRollServiceServer(s, &server{})
-	log.Printf("server listening at %v", lis.Addr())
-	if err := s.Serve(lis); err != nil {
+	if err := grpcServer.Serve(); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
