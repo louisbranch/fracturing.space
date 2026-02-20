0e1c64cc5813f2aef57711ccd3d76c9ee7eb0651
Louis Branch <branch3e8@gmail.com>
2026-02-19 19:33:06 -0500

    chore: collapse sqlite migrations into clean baselines

--- internal/services/ai/storage/sqlite/migrations/004_agent_provider_grant_reference.sql
+++
@@ -1,8 +0,0 @@
--- +migrate Up
-
-ALTER TABLE ai_agents ADD COLUMN provider_grant_id TEXT NOT NULL DEFAULT '';
-
-CREATE INDEX ai_agents_owner_provider_grant_id_idx ON ai_agents(owner_user_id, provider_grant_id);
-
--- +migrate Down
-DROP INDEX IF EXISTS ai_agents_owner_provider_grant_id_idx;
